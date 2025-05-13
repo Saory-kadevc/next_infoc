aca60cc4b3ab9c37716b4cb51557d9f995c757d7
Saory-kadevc <nicolasdesouzapereira830@gmail.com>
2025-05-13 19:14:30 +0000

    13/05

--- src/pages/useeffects.js
+++ src/pages/useeffects.js
@@ -1,21 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 
 export default function Useeffects() {
     const [titulo, settitulo] = useState("useeffects")
+    const [numero, setNumero ] = useState(0);
     setTimeout(() => {
             document.title = "Bem vindo ao Use-Effects"
             settitulo(mudatitulo())
         }, 3000
     )
+    useEffect(()=>{
+        console.log("Titulo alterado");
+},[titulo])
     return <>
         <Container className="text-center">
-            <h1>{titulo ===""?" Bem vindo":titulo}</h1>
+            <h1>{titulo ===""?" Bem vindo":titulo} : {numero}</h1>
             <p> </p>
+            <input type="button" onClick={()=>setNumero(SomaNumero(numero))} value={"soma numero"}></input>
         </Container>
     </>
 }
 
 function mudatitulo() {
-    return  "bem vindo ao Use-Effects"
+    return  "bem vindo ao Use-Effects";
+    
 }
+ function SomaNumero(Number) {
+
+    Number++;
+return Number;
+
+ }
